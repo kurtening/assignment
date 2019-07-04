6c715437a25ade132665f93557588e500aabbdfa
kurtening <kurly28@gmail.com>
2019-07-05 06:45:55 +0800

    Layout the pages

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.variable-width').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+});
+});
+
+function hidePass() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
